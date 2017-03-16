b9bfa123f22eac389abd5a990891a04ac487f392
pescel <28l.pesce@gmail.com>
2017-03-16 14:45:24 -0600

    update date filter to sort oldest and newest

--- public/app.js
+++ public/app.js
@@ -8,9 +8,9 @@ const loadDb = () => {
     headers: {
       'content-type': 'application/json',
     },
-  });
+  })
   .then(response => response.json())
-  .then(response => displayFolders(response));
+  .then(response => displayFolders(response))
 }
 loadDb();
 
@@ -18,38 +18,37 @@ const displayFolders = (folderArray) => {
   let list = document.querySelector('.folder-list');
   folderArray.map(folder => {
     return list.innerHTML = list.innerHTML + `<li><button class='folder-btn' id=${folder.id}>${folder.name}</button></li>`;
-  });
+  })
 }
 
 const displayUrls = (urlArray) => {
   let urlList = document.querySelector('.url-list');
   urlList.innerHTML = '';
   urlArray.map(url => {
-    console.log(url)
+    // console.log(url)
     return urlList.innerHTML = urlList.innerHTML + `<li><a href='http://${url.url}' target='_blank' class='url-btn'>${url.id}</a><p>${url.date}</p></li>`;
-  });
+  })
 }
 
 document.querySelector('.folder-submit-btn').addEventListener('click', () => {
-  let input = document.querySelector('.folder-input').value;
-  let list = document.querySelector('.folder-list');
+  let input = document.querySelector('.folder-input').value
+  let list = document.querySelector('.folder-list')
 
-  saveFolder(input);
-});
+  saveFolder(input)
+})
 
 document.querySelector('.folder-list').addEventListener('click', (e) => {
   folderId = e.target.id;
-  console.log(e.target);
-  console.log(folderId);
+  // console.log(e.target);
+  // console.log(folderId);
   loadUrls(e.target.id);
   // let main = document.querySelector('.main');
   // main.innerHTML = main.innerHTML + `<h2>${e.target.innerText}</h2>`
-});
+})
 
 const loadUrls = (folderId, filter) => {
-  console.log('#loadUrls');
-  console.log(folderId);
-  console.log('filter', filter);
+  // console.log(folderId)
+  // console.log('filter', filter)
   if(folderId){
     let url = 'http://localhost:3000/api/folders/' + folderId;
     fetch(url, {
@@ -57,34 +56,39 @@ const loadUrls = (folderId, filter) => {
       headers: {
         'content-type': 'application/json',
       },
-    });
+    })
     .then(response => response.json())
     .then((response) => {
-      if (filter == 'date'){
-        console.log('url array', response);
-        response = sortByDate(response)
+      if (filter == 'newest'){
+        response = sortByDate(response, filter)
+      }else if (filter == 'oldest'){
+        response = sortByDate(response, filter)
       } else if (filter == 'popularity') {
         response = sortByPopularity(response)
       }
-      displayUrls(response);
-    });
+      displayUrls(response)
+    })
   }
 }
 
-const sortByDate = (urls) => {
+const sortByDate = (urls, filter) => {
   let sortedUrls = urls.sort((a, b) => {
-    return new Date(a.date).getTime() - new Date(b.date).getTime()
-  });
+    if(filter == 'newest') {
+      return new Date(a.date).getTime() - new Date(b.date).getTime()
+    } else {
+      return new Date(b.date).getTime() - new Date(a.date).getTime()
+    }
+  })
   return sortedUrls;
 }
 
 document.querySelector('.url-submit-btn').addEventListener('click', () => {
   let urlInput = document.querySelector('.url-input').value;
-  let list = document.querySelector('.url-list');
+  let list = document.querySelector('.url-list')
 
-  saveUrl(folderId, urlInput);
-  loadUrls(folderId);
-});
+  saveUrl(folderId, urlInput)
+  loadUrls(folderId)
+})
 
 const saveFolder = (input) => {
   let url = 'http://localhost:3000/api/folders/';
@@ -96,14 +100,14 @@ const saveFolder = (input) => {
     },
     body: JSON.stringify({
       name: input,
-    });
-  });
+    })
+  })
   .then(response => response.json())
   .then(response => displayFolders([response]))
 }
 
 const saveUrl = (folderId, urlInput) => {
-  console.log('save')
+  // console.log('save')
   let input = document.querySelector('.url-input').value;
   if(folderId){
     let url = 'http://localhost:3000/api/folders/' + folderId;
@@ -115,13 +119,18 @@ const saveUrl = (folderId, urlInput) => {
       },
       body: JSON.stringify({
         url: urlInput,
-      });
-    });
+      })
+    })
     .then(response => response.json())
     .then(response => console.log(response))
   }
 }
 
-document.querySelector('.date-filter-btn').addEventListener('click', (e) => {
-  loadUrls(folderId, 'date');
-});
+document.querySelector('.newest-date-btn').addEventListener('click', (e) => {
+  loadUrls(folderId, 'newest');
+})
+
+document.querySelector('.oldest-date-btn').addEventListener('click', (e) => {
+  console.log('click')
+  loadUrls(folderId, 'oldest');
+})
